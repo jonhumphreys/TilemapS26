2bd41c6477c10f1e0050038969165bd3f6354faf
Jon Humphreys <jonhumphreys@chapman.edu>
2026-04-17 21:57:19 -0700

    Player input, movement, and animations for idle and running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,57 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public Rigidbody2D Rigidbody2D;
+    public Animator Animator;
+    
+    private Vector2 moveDirection;
+    
+    public void OnMove(InputValue value)
     {
-        
+        moveDirection = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        FaceCorrectDirection();
+        Animate();
+        Move();
+    }
+
+    private void Move()
+    {
+        Rigidbody2D.linearVelocity = moveDirection * GameParameters.PlayerSpeed;
+    }
+
+    private void Animate()
+    {
+        Animator.SetFloat("horizontal", Mathf.Abs(moveDirection.x));
+        Animator.SetFloat("vertical", Mathf.Abs(moveDirection.y));
+    }
+
+    private void FaceCorrectDirection()
+    {
+        if (IsNotFacingCorrectDirection())
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private bool IsNotFacingCorrectDirection()
+    {
+        if (moveDirection.x > 0 && transform.localScale.x < 0 || moveDirection.x < 0 && transform.localScale.x > 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
