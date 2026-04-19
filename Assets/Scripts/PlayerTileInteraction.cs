a1f4be14c32c7e6a2ebaab0da39af0ea10d528e5
Jon Humphreys <jonhumphreys@chapman.edu>
2026-04-18 19:21:19 -0700

    Added pickup/drop

--- Assets/Scripts/PlayerTileInteraction.cs
+++ Assets/Scripts/PlayerTileInteraction.cs
@@ -5,8 +5,56 @@ using UnityEngine.Tilemaps;
 public class PlayerTileInteraction : MonoBehaviour
 {
     public Tilemap AutoCollectTilemap;
+    public Tilemap InteractTilemap;
     public PlayerHealth PlayerHealth;
-    
+    public TileBase TileToPlace;
+
+    public void OnPickup()
+    {
+        PickupTile();
+    }
+
+    private void PickupTile()
+    {
+        if (InteractTilemap == null)
+        {
+            return;
+        }
+
+        Vector3Int cellPosition = InteractTilemap.WorldToCell(transform.position);
+        InteractTilemap.SetTile(cellPosition, null);
+    }
+
+    public void OnDrop()
+    {
+        DropTile();
+    }
+
+    private void DropTile()
+    {
+        if (InteractTilemap == null)
+        {
+            return;
+        }
+        if (TileToPlace == null)
+        {
+            return;
+        }
+        
+        Vector3Int cellPosition = InteractTilemap.WorldToCell(transform.position);
+        if (IsCellEmpty(cellPosition))
+        {
+            InteractTilemap.SetTile(cellPosition, TileToPlace);
+        }
+    }
+
+    private bool IsCellEmpty(Vector3Int cellPosition)
+    {
+        if (InteractTilemap.GetTile(cellPosition) == null)
+            return true;
+        return false;
+    }
+
     public void OnTriggerStay2D(Collider2D other)
     {
         RemoveAutoCollectTileAtPlayerPosition();
